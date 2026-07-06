2fcb29f179192040be55e5e5ce46e2dbecc6370a
matias <matiasrios@liceovvh.cl>
2026-07-06 13:00:26 -0400

    yup

---
+++ python/fundamentos/extras/sistema_usuarios/usuario.py
@@ -0,0 +1,7 @@
+from conexion import Conexion
+
+class usuario:
+    def __init__(self, nombre_usuario, contrasena, tipo_usuario):
+        nombre_usuario = nombre_usuario
+        contrasena = contrasena
+        tipo_usuario = tipo_usuario
