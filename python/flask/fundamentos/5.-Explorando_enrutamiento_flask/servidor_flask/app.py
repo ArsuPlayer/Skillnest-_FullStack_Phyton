e010dd01596307d3e3cae54f9175ecd4625d9bd2
isi <isidoravalenzuela@liceovvh.cl>
2026-07-13 10:31:37 -0400

    update

---
+++ python/flask/fundamentos/5.-Explorando_enrutamiento_flask/servidor_flask/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+app = Flask(__name__)
+
+# Ruta raíz - Página de inicio
+
+# Ruta genérica para explorar enrutamiento
+
+# Rutas dinámicas para personalización
+
+# Ruta que repite un mensaje varias veces
+
+# BONUS: Página de error personalizada si el usuario ingresa una ruta inexistente
+
+# Ejecuta el servidor
+if __name__ == "__main__":
+    app.run(debug=True)
