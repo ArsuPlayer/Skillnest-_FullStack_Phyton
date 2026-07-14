12936c6f66eaae09c9dd0ab0f7f169a33d238f4b
Matías <matiasrios@liceovvh.cl>
2026-07-14 12:58:18 -0400

    update

--- python/flask/fundamentos/5.-Explorando_enrutamiento_flask/servidor_flask/app.py
+++ python/flask/fundamentos/5.-Explorando_enrutamiento_flask/servidor_flask/app.py
@@ -2,7 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 # Ruta raíz - Página de inicio
-
+@app.route("/")
+def hola_mundo()
+    retur
 # Ruta genérica para explorar enrutamiento
 
 # Rutas dinámicas para personalización
